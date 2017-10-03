f9b117261255288e75c56560b5e7ab5be612f1be
William Sprent <bsprent@gmail.com>
2017-10-03 19:15:34 +0200

    initial commit

---
+++ src/edu/aa12/BnBNode.java
@@ -0,0 +1,41 @@
+package edu.aa12;
+
+/**
+ * A node in the branch-and-bound tree.
+ */
+public class BnBNode{
+    public double lowerBound;
+    public final BnBNode parent;
+    /** The depth in the bnb-tree*/
+    public final int depth;
+    /** The number of edges defined on the path to the root */
+    public final int edgesDefined;
+    public final Edge edge;
+    /** True if <code>edge</code> is included, false otherwise. */
+    public final boolean edgeIncluded;
+    public final double costOfIncludedEdges;
+
+    public BnBNode(BnBNode parent, Edge edge, boolean edgeIncluded, Graph graph){
+        this.parent = parent;
+        this.depth = (parent==null)?0:(parent.depth+1);
+        int edgeInc = edgeIncluded?1:0;
+        this.edgesDefined = (parent==null)?(edgeInc):(parent.edgesDefined+edgeInc);
+
+        this.edge = edge;
+        this.edgeIncluded = edgeIncluded;
+
+        double cost = 0.0;
+        if (parent!=null) cost += parent.costOfIncludedEdges;
+        if (edgeIncluded) cost += graph.getLength(edge);
+        this.costOfIncludedEdges = cost;
+    }
+
+    /** Return a string-representation of a node. Convenient for debugging. */
+    public String toString(){
+        if(parent==null) return "";
+        String spaces = "";
+
+        for(int i=0;i<depth;i++) spaces+=" ";
+        return parent.toString()+"\n"+spaces+String.format("BnBNode[%s,%b]",edge.toString(),edgeIncluded);
+    }
+}
